c7e3b67b85508ef1c4d223e2ea074600f09ad165
Adrien <ad.tranchand@gmail.com>
2024-07-18 11:45:19 +0200

    backend users/signup creation in routes commit 9

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+require("../models/connection");
+
+const User = require("../models/users");
+const Tweet = require("../models/tweets");
+
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+    if (!checkBody(req.body, ["username", "password"])) {
+        res.json({ result: false, error: "Missing or empty fields" });
+        return;
+    }
+
+    // Check if the user has not already been registered
+    User.findOne({ username: req.body.username }).then((data) => {
+        if (data === null) {
+            const hash = bcrypt.hashSync(req.body.password, 10);
+            const token = uid2(32);
+
+            const newUser = new User({
+                name: req.body.name,
+                username: req.body.username,
+                password: hash,
+                token: token,
+            });
+
+            newUser.save().then((newDoc) => {
+                res.json({ result: true, token: newDoc.token });
+            });
+        } else {
+            // User already exists in database
+            res.json({ result: false, error: "User already exists" });
+        }
+    });
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
 });
 
 module.exports = router;
