3da87b1323364e806e64c8d960556a497d9c5d85
Adrien <ad.tranchand@gmail.com>
2024-07-18 12:35:42 +0200

    backend tweets/updateTweet creation in routes commit 12

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 require("../models/connection");
 
 const Tweet = require("../models/tweets");
 const { checkBody } = require("../modules/checkBody");
 
-router.post("/postTweet", (req,res) => {
+router.post("/postTweet", (req, res) => {
     if (!checkBody(req.body, ["username", "password"])) {
         res.json({ result: false, error: "Missing or empty fields" });
         return;
@@ -14,11 +14,24 @@ router.post("/postTweet", (req,res) => {
     const newTweet = new Tweet({
         username: req.body.username,
         tweetContent: req.body.tweetContent,
-        publicationDate: moment(Date.now).format('YYYY-MM-DD HH:mm'),
-    })
-    
-    newTweet.save().then(data => {
-        res.json({tweet: data});
+        publicationDate: moment(Date.now).format("YYYY-MM-DD HH:mm"),
+    });
+
+    newTweet.save().then((data) => {
+        res.json({ tweet: data });
+    });
+});
+
+router.post("/updateTweet", (req, res) => {
+    Tweet.findById(req.body._id).then((data) => {
+        if (data) {
+            res.json({ result: true, tweetContent: data.tweetContent });
+        } else {
+            res.json({
+                result: false,
+                error: "Tweet not found you damn fool !",
+            });
+        }
     });
 });
 
